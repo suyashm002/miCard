eb7b80a7205bf1abfa538cf754a6ddd3bfc35c60
suyashmishra <Holikabhai"123>
2022-07-08 12:45:03 +0530

    Flutter project starting for visiting card

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+      MyApp()
   );
 }
+  class MyApp extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+  return MaterialApp(
+  home: Scaffold(
+  backgroundColor: Colors.teal,
+  body: SafeArea(
+    child: Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 50.0,
+          backgroundImage: AssetImage('images/suyash.jpeg'),
+        ),
+        Text('Suyash Mishra',style: TextStyle(
+          fontFamily: 'Pacifico',
+          fontSize: 40.0,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+        Text('Flutter Developer',
+        style: ,)
+      ],
+    ),
+  ),
+  ),
+  );
+
+    }
+  }
